87010c8766c99e41075e6ea9edb31b43643ce225
Mailen <mailen.mancuso@nan-labs.com>
2017-07-26 10:41:53 -0300

    throws error: rows

--- spec/game_of_life/trainingSpec.js
+++ spec/game_of_life/trainingSpec.js
@@ -1,12 +1,13 @@
 var GameOfLife = require('../../lib/GameOfLife');
 
 describe("Game Of Life", function(){
+
 	it("should be a function", function(){
 		expect(typeof GameOfLife).toBe("function");
 	})
 
 	it("should be an instance of Game Of Life", function(){
-		let gameOfLife = new GameOfLife(); 
+		let gameOfLife = new GameOfLife(1,2); 
 		expect(gameOfLife instanceof GameOfLife).toBe(true);
 	})
 
@@ -14,6 +15,15 @@ describe("Game Of Life", function(){
 		let gameOfLife = new GameOfLife(3,3);
 		expect(gameOfLife.sizeX).toBe(3);
 		expect(gameOfLife.sizeY).toBe(3);
+	})
+
+	it("should fail if rows are not set as numbers and smaller than one", function(){
+		try {
+			let gameOfLife = new GameOfLife("blabla",9);
+		}
+		catch(e) {
+			expect(e instanceof TypeError).toBe(true);
+		}
 	})	
 })
 
