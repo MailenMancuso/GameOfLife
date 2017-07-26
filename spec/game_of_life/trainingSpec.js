e0ffd1a512ebae70b1fe392eb22854ab779b4e2c
Mailen <mailen.mancuso@nan-labs.com>
2017-07-26 10:08:30 -0300

    test done

--- spec/game_of_life/trainingSpec.js
+++ spec/game_of_life/trainingSpec.js
@@ -1,5 +1,7 @@
 var gameOfLife = require('../../lib/gameOfLife');
 
 describe("Game Of Life", function(){
-	
+	it("should be a function", function(){
+		expect(typeof gameOfLife).to.equal("function");
+	})	
 })
