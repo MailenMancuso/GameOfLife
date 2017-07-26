9819d90337f5f4b5bfae6a695b2127fce4af7b8c
Mailen <mailen.mancuso@nan-labs.com>
2017-07-26 10:19:35 -0300

    is an instance

--- spec/game_of_life/trainingSpec.js
+++ spec/game_of_life/trainingSpec.js
@@ -1,7 +1,14 @@
-var gameOfLife = require('../../lib/GameOfLife');
+var GameOfLife = require('../../lib/GameOfLife');
 
 describe("Game Of Life", function(){
 	it("should be a function", function(){
-		expect(typeof gameOfLife).toBe("function");
+		expect(typeof GameOfLife).toBe("function");
+	})
+
+	it("should be an instance of Game Of Life", function(){
+		let gameOfLife = new GameOfLife(); 
+		expect(gameOfLife instanceof GameOfLife).toBe(true);
 	})	
 })
+
+
